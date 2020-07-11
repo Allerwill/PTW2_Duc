27e2f33c9a410ba3f5350f591783aff2ec5c8b8c
Lê Trí Đức <33621714+Allerwill@users.noreply.github.com>
2020-07-11 11:37:07 +0700

    updated

--- public/App.jsx
+++ public/App.jsx
@@ -6,8 +6,20 @@ function tick() {
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+
+  var name =(
+    <div>
+    <h1>Chào bạn, {name}</h1>
+    </div>
+  );
   // highlight-next-line
-  ReactDOM.render(element, document.getElementById('root'));
+  ReactDOM.render(
+    element,
+
+    document.getElementById('root')
+  );
+  // ReactDOM.render(name, document.getElementById('root'));
+
 }
 
 setInterval(tick, 1000);
