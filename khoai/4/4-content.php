6e0002b4eed6beda391ea4e31d1768d424e0d2aa
Lê Trí Đức <33621714+Allerwill@users.noreply.github.com>
2020-08-04 17:28:22 +0700

    updated

--- khoai/4/4-content.php
+++ khoai/4/4-content.php
@@ -1,15 +1,15 @@
 <?php
-    $url_host = $_SERVER['HTTP_HOST'];
+$url_host = $_SERVER['HTTP_HOST'];
 
-    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 
-    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-    preg_match_all($pattern_uri, __DIR__, $matches);
+preg_match_all($pattern_uri, __DIR__, $matches);
 
-    $url_path = $url_host . $matches[1][0];
+$url_path = $url_host . $matches[1][0];
 
-    $url_path = str_replace('\\', '/', $url_path);
+$url_path = str_replace('\\', '/', $url_path);
 ?>
 
 <div class="type-4">
@@ -20,19 +20,19 @@
             <div class="swiper-slide">
                 <div class="img-title">Residential care <br>project in Paris</div>
                 <div class="office">Architecture</div>
-                <div class="more-info"><img src="images/zoom.png"  style="width: 15px;"></div>
+                <div class="more-info"><img src="images/zoom.png" style="width: 15px;"></div>
                 <div class="cover-img"><img src="images/1.jpg" alt=""></div>
             </div>
             <div class="swiper-slide">
                 <div class="img-title">Concert hall in <br>New york</div>
                 <div class="office">Interior</div>
-                <div class="more-info"><img src="images/zoom.png"  style="width: 15px;"></div>
-                <div class="cover-img"><img src="images/2.jpg"  alt=""></div>
+                <div class="more-info"><img src="images/zoom.png" style="width: 15px;"></div>
+                <div class="cover-img"><img src="images/2.jpg" alt=""></div>
             </div>
             <div class="swiper-slide">
                 <div class="img-title">Modern hotel in <br>London</div>
                 <div class="office">Design</div>
-                <div class="more-info"><img src="images/zoom.png"  style="width: 15px;"></div>
+                <div class="more-info"><img src="images/zoom.png" style="width: 15px;"></div>
                 <div class="cover-img"><img src="images/3.jpg" alt=""></div>
             </div>
             <div class="swiper-slide">
@@ -48,8 +48,10 @@
                 <div class="cover-img"><img src="images/5.jpg" alt=""></div>
             </div>
         </div>
+
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
+
     </div>
 
     <div class="w3-content w3-display-container" style="text-align: -webkit-center;">
@@ -73,18 +75,22 @@
         function showDivs(n) {
             var i;
             var x = document.getElementsByClassName("mySlides");
-            if (n > x.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = x.length}
+            if (n > x.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = x.length
+            }
             for (i = 0; i < x.length; i++) {
                 x[i].style.display = "none";
             }
-            x[slideIndex-1].style.display = "block";
+            x[slideIndex - 1].style.display = "block";
         }
 
-        $('.more-info').click(function(){
+        $('.more-info').click(function () {
             $('body').addClass("enable")
         });
-        $('.close, .fake-body').click(function(){
+        $('.close, .fake-body').click(function () {
             $('body').removeClass("enable")
         });
     </script>
