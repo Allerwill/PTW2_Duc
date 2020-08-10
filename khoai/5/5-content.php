1d61c2da7ac900a1bbbbd7075c1caf61c1f6156d
Lê Trí Đức <33621714+Allerwill@users.noreply.github.com>
2020-08-10 21:19:27 +0700

    Updated

--- khoai/5/5-content.php
+++ khoai/5/5-content.php
@@ -1,26 +1,24 @@
 <?php
-    $url_host = $_SERVER['HTTP_HOST'];
+$url_host = $_SERVER['HTTP_HOST'];
 
-    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 
-    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
-    preg_match_all($pattern_uri, __DIR__, $matches);
+preg_match_all($pattern_uri, __DIR__, $matches);
 
-    $url_path = $url_host . $matches[1][0];
+$url_path = $url_host . $matches[1][0];
 
-    $url_path = str_replace('\\', '/', $url_path);
+$url_path = str_replace('\\', '/', $url_path);
 ?>
 
 <div class="type-5">
-    <div class="container">
-
-        <div class="background-cover-number">
-
-            <div class="number">12</div>
-            <h4 class="experience-info wow fadeInRight" style="visibility: visible; animation-name: fadeInRight;"><span class="text-primary">Years of successful work</span><br> in the market</h4>
-
-        </div>
+    <div>
+        <div class="number">12<span style="font-size: 30px">Years of successful work</span>
+        <div class="number">12<span style="font-size: 30px">Years of successful work</span>
+        <div class="number">12<span style="font-size: 30px">Years of successful work</span>
+            <span style="font-size: 30px; color: black;">in the market</span></div>
 
     </div>
+
 </div>
